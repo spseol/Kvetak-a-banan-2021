0d0f5e05742cc9741e3b1f444ad96b99cbb49610
Marrek Nožka <nozka@spseol.cz>
2021-11-11 14:05:54 +0100

    zacates session

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = b'\xe3\x84t\x8b\x02\x1c\xfb\x82PH\x19\xe8\x98\x05\x90\xa8\xc83\xf1\xe2\xf4v\xfe\xf0'b'\xe3\x84t\x8b\x02\x1c\xfb\x82PH\x19\xe8\x98\x05\x90\xa8\xc83\xf1\xe2\xf4v\xfe\xf0'
+"""
+* secret_key se generuje nejlépe pomocí os.urandom(24)
+* ale obecně je to prostě velké náhodné číslo
+* proměnnou secrec_key nikdi nikdy nidky nesdílím v repositáři!!! tak jako teď :)
+"""
+app.secret_key = (
+    b"\xe3\x84t\x8b\x02\x1c\xfb\x82PH\x19\xe8\x98\x05\x90\xa8\xc83\xf1\xe2\xf4v\xfe\xf0"
+    b"\xe3\x84t\x8b\x02\x1c\xfb\x82PH\x19\xe8\x98\x05\x90\xa8\xc83\xf1\xe2\xf4v\xfe\xf0"
+)
+# app.secret_key = os.urandom()
 
 
 @app.route("/")
@@ -11,19 +20,19 @@ def index():
 
 @app.route("/abc/", methods=["GET"])
 def abc():
-    session['user'] = 'karel'
+    session["user"] = "karel"
     try:
-        x = request.args.get("x") 
+        x = request.args.get("x")
         y = request.args.get("y")
         soucet = int(x) + int(y)
     except TypeError:
         soucet = None
     except ValueError:
         soucet = "Nedělej si srandu!!!"
-    
-    slovo = request.args.get('slovo')
+
+    slovo = request.args.get("slovo")
     if slovo:
-        session['slovo'] = slovo
+        session["slovo"] = slovo
 
     return render_template("abc.html.j2", soucet=soucet)
 
@@ -38,7 +47,7 @@ def abc_post():
     return redirect(url_for("abc"))
 
 
-@app.route("/banany/<parametr>")
+@app.route("/banany/<path:parametr>/")
 def banany(parametr):
     return render_template("banany.html.j2", parametr=parametr)
 
@@ -46,3 +55,18 @@ def banany(parametr):
 @app.route("/kvetak/")
 def kvetak():
     return render_template("kvetak.html.j2")
+
+
+@app.route("/login/", methods=["GET"])
+def login():
+    return render_template("login.html.j2")
+
+
+@app.route("/login/", methods=["POST"])
+def login_post():
+    pass
+    pass
+    pass
+    pass
+    pass
+    return redirect(url_for('login'))
